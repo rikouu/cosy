38952ce6b8fe72a2f91d3ec038c5af1f468fa0b3
Loren <mwl@live.com>
2019-06-26 22:08:02 +0800

    还是 这个顺眼一点

--- resources/views/articles/template/image.blade.php
+++ resources/views/articles/template/image.blade.php
@@ -7,11 +7,11 @@
 @section('content')
     <main class="py-4 py-md-5">
         <div class="container">
-            @includeWhen($showSideBar, 'articles.partials._breadcrumbs') 
+            @includeWhen($showSideBar, 'articles.partials._breadcrumbs')
 
             @if (!empty($content->image))
             <div class="post-cover list-rounded overlay-hover mb-2 mb-md-5">
-                <div class="d-flex flex-fill bg-img bg-dark text-center h-v-40" style="background-image: url('{{ $content->image }}');">
+                <div class="d-flex flex-fill bg-img bg-dark text-center h-v-33" style="background-image: url('{{ $content->image }}');">
                     <div class="d-flex flex-fill flex-column align-items-center bg-dark-overlay py-12">
                         <div class="m-auto p-4">
                             <h1 class="h3 text-white">{{ $article->title }}</h1>
@@ -28,13 +28,13 @@
                                 <a href="https://demo.nicetheme.xyz/panda-pro-style-one/category/shenghuo" target="_blank"><span class="badge badge-primary">生活</span></a>
                             </div>
                             <h1 class="h3 text-white">{{ $article->title }}</h1>
-                            
+
                             <div class="meta text-xs text-muted">
                                 <time class="d-inline-block">2019-06-20 1:22:38</time>
                                 <span class="mx-1"><i class="text-md iconfont icon-eye-line mx-1"></i><small>345</small></span>
                                 <a class="mx-1" href="#comments"><i class="text-md iconfont icon-chat--line mx-1"></i><small>0</small></a>
                                 <a class="btn-like btn-link-like  mx-1" href="javascript:;" data-action="like" data-id="4615"><i class="text-md iconfont icon-thumb-up-line mx-1"></i><small class="like-count">3</small></a>
-                            </div>                
+                            </div>
                         </div>
                     </div>
                 </div> --}}
